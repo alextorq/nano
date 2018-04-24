13179447e1d121f8a82e90703c923dd2065276d2
alex <xxxxx_07@inbox.ru>
2018-04-24 20:53:52 +0300

    add mongobg client

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express        = require('express');
 const opn            = require('opn');
-const MongoClient    = require('mongodb').MongoClient;
+const mongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
-
+const levels         = require('./module/data/levels/levels.js');
 const port = 8000;
+const jsonParser = bodyParser.json();
+
+let client;
+const levelsUrl = "mongodb://localhost:27017";
+const levelsDatabase = 'levels';
+
+
 
 app.use(express.static(__dirname + "/"));
 
@@ -12,7 +19,19 @@ app.listen(port, () => {
     opn('http://127.0.0.1:8000');
 });
 
-
-
-
-
+app.post("/data", jsonParser, function (request, response) { //Обработка запроса на аддресе
+    console.log('same body want your data');
+    mongoClient.connect(levelsUrl).
+    then((clientObj) => {
+        client = clientObj;
+        let db = client.db(levelsDatabase);
+        let collection = db.collection('levels');
+        return collection.find().toArray();
+    }).
+    then((results) => {
+        console.log(results);
+        client.close();
+        response.send(results);
+    }).
+    catch((err) => {console.log(err)});
+});
