f04001da0831f215d80c541c26e959a3df417e83
alex <xxxxx_07@inbox.ru>
2018-04-27 22:23:58 +0300

    add levels to database

---
+++ dataBase.js
@@ -0,0 +1,19 @@
+const mongoClient    = require('mongodb').MongoClient;
+const levels         = require('./module/data/levels/levels');
+
+
+let client;
+const levelsUrl = "mongodb://localhost:27017";
+const levelsDatabase = 'levels';
+
+mongoClient.connect(levelsUrl).
+then((clientObj) => {
+    client = clientObj;
+    let db = client.db(levelsDatabase);
+    let collection = db.collection('levels');
+    return collection.insertOne(levels);
+}).
+then(() => {
+    client.close();
+}).
+catch((err) => {console.log(err)});
